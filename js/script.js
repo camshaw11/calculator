7ac3a2e51c0faa9c04fbb835b15eb5fca35ae500
cameron <cameronshaw95@gmail.com>
2020-07-14 23:00:14 +1200

    folder structure and pseudocode

---
+++ js/script.js
@@ -0,0 +1,164 @@
+
+
+
+/////////-----------------------//////////
+
+// create variables to keep track of totals
+// we need to keep track of the running total
+// and the previous operator
+// and the temporary value 
+
+
+
+// create event listener to listen for clicks
+// event bubbling on all buttons
+
+/////////-----------------------//////////
+
+
+
+
+
+/////////-----------------------//////////
+
+// run function on button click //
+
+
+// if value clicked on is not a number
+// run function to handle the symbol
+
+
+// if value clicked on is a number 
+// or if value is a decimal point
+// run function to handle value
+
+
+// call function to clear the screen
+
+
+/////////-----------------------//////////
+
+
+
+
+
+
+
+/////////-----------------------//////////
+
+// run function if value is a number or decimal //
+// if buffer number on screen is 0
+// buffer is now assigned the value that was clicked
+
+// else we add the value to the buffer 
+
+/////////-----------------------//////////
+
+
+
+
+
+
+
+/////////-----------------------//////////
+
+// run function if value is a special symbol AC, =, <- //
+
+// switch statement to check value
+
+// if value clicked is AC //
+
+// our total is cleared back to 0
+// we set our previous operator to null
+// set our buffer back to 0
+
+
+// if value clicked is '=' //
+ 
+// make sure last click was not null
+// if it was null , end switch
+// otherwise run calculate function
+
+
+// if value clicked is <- //
+
+
+// otherwise default option is to run math function
+// end switch
+
+
+/////////-----------------------//////////
+
+
+
+
+
+
+
+
+/////////-----------------------//////////
+
+// run function to handle math
+
+/////////-----------------------//////////
+
+
+
+
+
+
+
+
+/////////-----------------------//////////
+
+// run calculate function
+
+// if clicked value was +
+// add the current number to running total
+
+
+// if clicked value was -
+// minus current number from running total
+
+
+// if clicked value was X
+// multiply current number to the running total
+
+
+// if clicked value was /
+// divide current number by the running total
+
+
+/////////-----------------------//////////
+
+
+
+/////////-----------------------//////////
+
+// run re render function
+// sets the text on screen to the current value
+
+/////////-----------------------//////////
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

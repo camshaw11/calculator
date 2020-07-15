63c0584a9886d657254e72575798139da0a1d9ca
cameron <cameronshaw95@gmail.com>
2020-07-16 10:21:48 +1200

    add function to handle values

--- js/script.js
+++ js/script.js
@@ -8,29 +8,47 @@
 // 1. and the previous operator
 // 1. and the temporary value 
 
+let screen = document.querySelector(".screen");
+let btnGroup = document.querySelector(".button-wrapper");
+let runningTotal = 0;
+let previousOperator = 0;
+let temp = "0";
+
 
 
 // create event listener to listen for clicks
 // event bubbling on all buttons
+btnGroup.addEventListener("click", buttonClicked);
 
 /////////-----------------------//////////
-
-
 
 
 
 /////////-----------------------//////////
 
 // run function on button click //
+function buttonClicked(e) {
 
+    let value = e.target.innerText;
 
-// if value clicked on is not a number
-// run function to handle the symbol
+    // if value clicked on is a number 
+    // or if value is a decimal point
+    // run function to handle value
+    if (value === "." || !isNaN(value)){
+        handleNumber(value)
+        console.log(value);
+    }
 
+    // if value clicked on is not a number
+    // run function to handle the symbol
+    else if (!parseInt(value)) {
+        handleSymbol(value)
+        console.log(value);
+    }
 
-// if value clicked on is a number 
-// or if value is a decimal point
-// run function to handle value
+    // call function to re render screen
+    reRender();
+}
 
 
 // call function to re render screen
@@ -40,22 +58,21 @@
 
 
 
-
-
-
-
 /////////-----------------------//////////
 
 // run function if value is a number or decimal //
-// if temp number is 0
-// temp is now assigned the value that was clicked
-
-// else we append the value to the buffer 1 1 = 11
+function handleNumber(value) {
+    // if temp number is 0
+    // temp is now assigned the value that was clicked  
+    if (temp === "0") {
+        temp = value;
+    } else {
+        // else we append the value to the temp 1 1 = 11
+        temp += value;
+    }
+}
 
 /////////-----------------------//////////
-
-
-
 
 
 
@@ -63,47 +80,51 @@
 /////////-----------------------//////////
 
 // run function if value is a special symbol AC, =, <- //
+function handleSymbol(value) {
+    // switch statement to check value
+    switch (value) {
+        // if value clicked is AC //
+        case "AC":
+            // our total is cleared back to 0
+            runningTotal = 0;
+            // we set our previous operator to null
+            previousOperator = null;
+            // set our temp back to 0
+            temp = "0";    
+            break;
+        // if value clicked is '=' //
+        case "=":
+            // make sure last click was not null
+            // if it was null , end switch
 
-// switch statement to check value
+            // otherwise run calculate function with temp value 
 
-// if value clicked is AC //
+            // set previous operator back to null because equation has finished
 
-// our total is cleared back to 0
-// we set our previous operator to null
-// set our temp back to 0
-
-
-// if value clicked is '=' //
- 
-// make sure last click was not null
-// if it was null , end switch
-
-// otherwise run calculate function with temp value 
-
-// set previous operator back to null because equation has finished
-
-// temp value now equals out running total
+            // temp value now equals out running total
+            break;
+        // otherwise default option is to run math function
+        default: 
+        doMath();
+    }
 
 
+}
 // if value clicked is <- //
 
 
-// otherwise default option is to run math function
-// end switch
 
 
 /////////-----------------------//////////
-
-
-
-
-
 
 
 
 /////////-----------------------//////////
 
 // run function to handle math
+function doMath() {
+
+}
 // store temp as integer so we dont lose it
 
 // if our current total is 0 change it the temp number
@@ -114,12 +135,8 @@
 
 
 // ready for next value to come in, set temp back to "0"
+
 /////////-----------------------//////////
-
-
-
-
-
 
 
 
@@ -150,6 +167,9 @@
 /////////-----------------------//////////
 
 // run re render function
+function reRender() {
+    screen.innerText = temp;
+}
 // sets the text on screen to the current value
 
 /////////-----------------------//////////
